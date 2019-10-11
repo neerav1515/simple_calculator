d41b9f95d7a82d179c58131d4f5ab25619d1b573
anishagg17 <43617894+anishagg17@users.noreply.github.com>
2019-10-11 17:19:07 +0530

    fixed extension used for making a python code
    extension should be .py instead of .md
    several indentation errors fixed

---
+++ calculator.py
@@ -0,0 +1,12 @@
+from math import *
+
+a=float(input("enter first number : "))
+b=input("enter operator + , - , * , / : ") 
+c=float(input("enter second number : "))
+
+print("result is : ") 
+if b=='+': print(a+c) 
+elif b=='-': print(a-c) 
+elif b=='': print(ac) 
+elif b=='/': print(a/c) 
+else: print("fool !! wrong operator")
